be76b11d0ebef4c67adffc54e76faa5e7de6cd71
Jetpack.JS <jetpack.js@gmail.com>
2025-10-18 20:10:26 +0800

    Add 2019-10-01 20:31:28

---
+++ ios-appstore-11/2019-10-01 20:31:28/index.js
@@ -0,0 +1,201 @@
+{
+  "properties": {
+    "version": "11.3.1568335001+sha.1c411ea2",
+    "tvAppEnabledStorefronts_11_0": [
+      "143441",
+      "143455",
+      "143460"
+    ],
+    "tvAppEnabledStorefronts_11_1": [
+      "143457",
+      "143456"
+    ],
+    "macauStorefrontIdentifier": "143515",
+    "suppressedReviewAdamIds": [
+      "1426355645"
+    ],
+    "localizations": {
+      "ca-es": {
+        "Ok": "Acceptar",
+        "AppStore.Preorders.Unsupported": "Per reservar aquesta app, actualitza iOS a la versió 11.2 o posterior."
+      },
+      "cs-cz": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Chcete-li si rezervovat tuto aplikaci, aktualizujte na iOS 11.2 nebo novější."
+      },
+      "da-dk": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Hvis du vil forudbestille denne app, skal du opdatere til iOS 11.2 eller en nyere version."
+      },
+      "de-ch": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Aktualisiere auf iOS 11.2 oder neuer, um diese App vorzubestellen."
+      },
+      "de-de": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Aktualisiere auf iOS 11.2 oder neuer, um diese App vorzubestellen."
+      },
+      "el-gr": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Για προπαραγγελία αυτής της εφαρμογής, πραγματοποιήστε αναβάθμιση σε iOS 11.2 ή μεταγενέστερη έκδοση."
+      },
+      "en-au": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "To pre-order this app, update to iOS 11.2 or later."
+      },
+      "en-ca": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "To pre-order this app, update to iOS 11.2 or later."
+      },
+      "en-gb": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "To pre-order this app, update to iOS 11.2 or later."
+      },
+      "en-ua": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "To pre-order this app, update to iOS 11.2 or later."
+      },
+      "en-uk": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "To pre-order this app, update to iOS 11.2 or later."
+      },
+      "en-us": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "To pre-order this app, update to iOS 11.2 or later."
+      },
+      "es-es": {
+        "Ok": "Aceptar",
+        "AppStore.Preorders.Unsupported": "Para reservar esta app, actualiza iOS a la versión 11.2 o posterior."
+      },
+      "es-mx": {
+        "Ok": "Aceptar",
+        "AppStore.Preorders.Unsupported": "Para realizar el pedido anticipado de esta app, actualiza a iOS 11.2 o posterior."
+      },
+      "fi-fi": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Jos haluat ennakkotilata tämän apin, päivitä versioon iOS 11.2 tai uudempaan."
+      },
+      "fr-ca": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Pour précommander cette app, effectuez la mise à jour vers iOS 11.2 ou une version ultérieure."
+      },
+      "fr-ch": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Pour précommander cette app, veuillez mettre à jour votre iOS vers la version 11.2 ou ultérieure."
+      },
+      "fr-fr": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Pour précommander cette app, veuillez mettre à jour votre iOS vers la version 11.2 ou ultérieure."
+      },
+      "hi-in": {
+        "Ok": "ठीक",
+        "AppStore.Preorders.Unsupported": "इस ऐप को प्री-ऑर्डर करने के लिए iOS 11.2 या बाद के संस्करण में अपडेट करें।"
+      },
+      "hr-hr": {
+        "Ok": "U redu",
+        "AppStore.Preorders.Unsupported": "Da biste prednaručili ovu aplikaciju, ažurirajte na sustav iOS 11.2 ili noviji."
+      },
+      "hu-hu": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Az alkalmazás előrendeléséhez frissítsen az iOS 11.2 vagy újabb verziójára."
+      },
+      "id-id": {
+        "Ok": "OKE",
+        "AppStore.Preorders.Unsupported": "Untuk memprapesan app ini, perbarui ke iOS 11.2 atau versi lebih baru."
+      },
+      "it-it": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Per poter prenotare questa app devi eseguire l’aggiornamento a iOS 11.2 o versioni successive."
+      },
+      "ja-jp": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "このAppを予約注文するには、iOS 11.2以降にアップデートしてください。"
+      },
+      "ko-kr": {
+        "Ok": "승인",
+        "AppStore.Preorders.Unsupported": "이 앱을 사전 주문하려면 iOS 11.2 이상으로 업데이트하십시오."
+      },
+      "ms-my": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Untuk membuat pra‑tempahan bagi app ini, kemas kini ke iOS 11.2 atau terkini."
+      },
+      "nl-nl": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Werk bij naar iOS 11.2 of later om deze app vooruit te bestellen."
+      },
+      "no-no": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "For å forhåndsbestille denne appen må du oppdatere til iOS 11.2 eller nyere."
+      },
+      "pl-pl": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Aby zamówić tę aplikację, uaktualnij iOS do wersji 11.2 lub nowszej."
+      },
+      "pt-br": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Para encomendar este app, atualize para o iOS 11.2 ou posterior."
+      },
+      "pt-pt": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Para reservar esta app, efetue a atualização para o iOS 11.2 ou posterior."
+      },
+      "ro-ro": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Pentru a precomanda această aplicație, trebuie să efectuați o actualizare la iOS 11.2 sau o versiune ulterioară."
+      },
+      "ru-ru": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Чтобы оформить предзаказ приложения, выполните обновление, установив iOS 11.2 или новее."
+      },
+      "sk-sk": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Ak si chcete predobjednať túto aplikáciu, aktualizujte na iOS 11.2 alebo novší."
+      },
+      "sv-se": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "För att förbeställa den här appen måste du uppdatera till iOS 11.2 eller senare."
+      },
+      "th-th": {
+        "Ok": "ตกลง",
+        "AppStore.Preorders.Unsupported": "หากต้องการสั่งซื้อแอพนี้ล่วงหน้า โปรดอัพเดทเป็น iOS 11.2 หรือใหม่กว่า"
+      },
+      "tr-tr": {
+        "Ok": "Tamam",
+        "AppStore.Preorders.Unsupported": "Bu uygulamayı ön sipariş vermek için iOS 11.2 veya üst sürümüne güncelleyin."
+      },
+      "uk-ua": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Щоб передзамовити програму, потрібна iOS 11.2 або новішої версії."
+      },
+      "vi-vi": {
+        "Ok": "OK",
+        "AppStore.Preorders.Unsupported": "Để đặt hàng trước app này, hãy nâng cấp lên iOS 11.2 hoặc mới hơn."
+      },
+      "zh-cn": {
+        "Ok": "好",
+        "AppStore.Preorders.Unsupported": "要预订此 App，请更新至 iOS 11.2 或更高版本。"
+      },
+      "zh-hans": {
+        "Ok": "好",
+        "AppStore.Preorders.Unsupported": "要预订此 App，请更新至 iOS 11.2 或更高版本。"
+      },
+      "zh-hant": {
+        "Ok": "好",
+        "AppStore.Preorders.Unsupported": "若要預訂此 App，請更新至 iOS 11.2 或更新版本。"
+      },
+      "zh-hk": {
+        "Ok": "好",
+        "AppStore.Preorders.Unsupported": "若要預訂此 App，請更新至 iOS 11.2 或更新版本。"
+      },
+      "zh-ma": {
+        "Ok": "好",
+        "AppStore.Preorders.Unsupported": "要预订此 App，请更新至 iOS 11.2 或更高版本。"
+      },
+      "zh-tw": {
+        "Ok": "好",
+        "AppStore.Preorders.Unsupported": "若要預訂此 App，請更新至 iOS 11.2 或更新版本。"
+      }
+    }
+  },
+  "js": "600ef6e6d9e2b23ec67bbb6ccde9e12c/app.js"
+}
