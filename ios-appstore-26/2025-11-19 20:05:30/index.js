61b4cb92be74ff928d091047368df8eddd3b450a
Jetpack.JS <jetpack.js@gmail.com>
2025-11-20 04:51:05 +0800

    Add ios-appstore-26/2025-11-19 20:05:30

---
+++ ios-appstore-26/2025-11-19 20:05:30/index.js
@@ -0,0 +1,1 @@
+{"properties":{"version":"25.2.0-12","tvAppEnabledStorefronts_11_0":["143441","143455","143460"],"tvAppEnabledStorefronts_11_1":["143457","143456"],"macauStorefrontIdentifier":"143515","spamBlockingExtensionsEditorialItemID":"1490737895","localizations":{"format":"json/multi-file","path":"localization"}},"js":null,"cacheKey":"25.2.0-12-93adc84c-d487-4dfb-aac6-a491d70784cd"}
